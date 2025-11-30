d3e2aa485919ed9b8a4f5bb34923bc33ca74940c
Subin Oh <117198327+subin930@users.noreply.github.com>
2025-11-30 21:16:32 +0900

    [Silver V] Title: 숫자 카드, Time: 912 ms, Memory: 153644 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.stream(br.readLine().split(" ")).forEach(x -> {set.add(Integer.parseInt(x));});
+
+        int M = Integer.parseInt(br.readLine());
+
+        int[] target = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for(int i = 0; i < M; ++i) {
+            if(set.contains(target[i])) bw.write("1 ");
+            else bw.write("0 ");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
