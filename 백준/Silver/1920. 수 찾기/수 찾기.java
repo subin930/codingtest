434088d03c1561ee090da9e4b5294f62af4e1e8c
Subin Oh <117198327+subin930@users.noreply.github.com>
2025-11-30 21:14:38 +0900

    [Silver IV] Title: 수 찾기, Time: 568 ms, Memory: 54208 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,35 +8,20 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        int[] a = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(a);
+        HashSet<Integer> set = new HashSet<>();
+        Arrays.stream(br.readLine().split(" ")).forEach(x -> {set.add(Integer.parseInt(x));});
 
         int M = Integer.parseInt(br.readLine());
 
         int[] target = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
         for(int i = 0; i < M; ++i) {
-            if(binarySearch(a, target[i]) >= 0) bw.write("1\n");
+            if(set.contains(target[i])) bw.write("1\n");
             else bw.write("0\n");
         }
 
         bw.flush();
         bw.close();
         br.close();
-    }
-
-    static int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] > target) right = mid - 1;
-            else left = mid + 1;
-        }
-
-        return -1;
     }
 }
